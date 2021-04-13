06e92efdc5e8a1322bdbbc43cfae614439bf436a
Reza Basereh <rzbasereh1@gmail.com>
2021-04-13 19:38:03 +0430

    fix #1: work with lists

---
+++ lab4/3/list.py
@@ -0,0 +1,35 @@
+# 3-1
+
+
+##### Part 1
+
+# initial new list 
+arr1 = []
+
+# append new data to list
+arr1.append('95105968')
+arr1.append('reza')
+
+print(arr1)
+
+##### Part 2
+
+arr2 = []
+arr2.append('95105965')
+arr2.append('ali')
+
+arr3 = []
+arr3.append('95105960')
+arr3.append('ahmad')
+
+wrapper = []
+wrapper.append(arr1)
+wrapper.append(arr2)
+wrapper.append(arr3)
+
+print(wrapper)
+
+##### Part 3 
+
+print(wrapper[0])
+print(wrapper[0][0])
