c77e7adb169854a93aa79c9b62910a74dd3bb1f2
Reza Basereh <rzbasereh1@gmail.com>
2021-04-28 01:43:41 +0430

    Add plot histogram of ApplicantIncome column

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # PythonLab_Group6
+
+## Lab 6
+
+### 3-matplotlib
+
+```bash
+    $ pip install pandas
+    $ python -m pip install -U matplotlib
+```
---
+++ lab6/3/3_1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+n_bins = 50
+headers = ['ApplicantIncome']
+
+df = pd.read_csv('train.csv')
+
+ApplicantIncome = df.ApplicantIncome.tolist()
+print(ApplicantIncome)
+
+plt.hist(ApplicantIncome, bins = n_bins)
+plt.show()
